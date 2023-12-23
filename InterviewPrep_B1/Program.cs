6eec94728ac057e19086da4df035c21b8d837a12
Dilsad <dilshadashraf52@gmail.com>
2023-12-23 23:59:10 +0530

    Add project files.

---
+++ InterviewPrep_B1/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterviewPrep_B1;//namespace /package
+
+Console.WriteLine("Hello, World!");
+
+// We can have any object name (it should not be the predefine keyword,or any existing name which have already used)
+TestClass obj = new TestClass();
+
+// With the object name you can access the members from the class
+
+obj.GetDetails();
+
+void GetData()
+{
+
+}
+
+GetData();
---
+++ InterviewPrep_B1/TestClass.cs
@@ -0,0 +1,74 @@
+﻿namespace InterviewPrep_B1
+{
+    // TestClass is class name
+    public class TestClass
+    {
+        // Data members
+        public int a = 10;
+        public string name, address;
+        public int MyProperty { get; set; }
+
+        //Methods
+        public void GetDetails()
+        {
+            Console.WriteLine("Test Message From GetDetails Method");
+        }
+
+        public void GetData()
+        {
+            Console.WriteLine("Test Message from GetData Method");
+        }
+
+        //constructor
+        public TestClass()
+        {
+                
+        }
+    }
+}
+
+/*
+ What is class? 
+
+-> User Define Data Type
+-> we can create class with uppercase and lower
+-> It is collection of members
+        - function member/ methods
+        - data members
+            - field (int a,b;)
+        - Properties
+        - Constructor
+
+
+
+-> We can create class using class keyword
+
+-> It should not be the predefine datatype or class (when you are going to create)
+
+
+2. What is default access of a class?
+-> internal
+
+3. Default scope of members in a class?
+-> Private
+
+
+
+
+Object
+=======
+1. What is object in C#?
+
+-> Object is instance of class
+-> If we want to access memebers (data members/methods etc.) of a class, 
+we can create object of that class 
+
+2. How to create object of a class?
+ClassName objName= new ClassName();
+
+or
+
+ClassName objName;
+objName = new ClassName();
+
+ */
